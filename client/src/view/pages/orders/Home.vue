8eae7ba6c8e416d433c8c3b7b3abfe420d23c172
Mahmud <ajaomahmud@gmail.com>
2021-03-23 07:16:25 +0100

    feat (Orders): add orders component views

---
+++ client/src/view/pages/orders/Home.vue
@@ -0,0 +1,107 @@
+<template>
+  <div>
+    <!--begin::Row-->
+    <div class="row mb-10">
+      <div class="col-lg-6 col-xl-4 mb-10">
+        <!--begin::Callout-->
+        <div class="card card-custom mb-2 bg-diagonal">
+          <div class="card-body">
+            <div
+              class="d-flex align-items-center justify-content-between p-4 flex-lg-wrap flex-xl-nowrap"
+            >
+              <div class="d-flex flex-column mr-5">
+                <router-link
+                  to="/orders/laboratory"
+                  class="h4 text-dark text-hover-primary mb-5"
+                >
+                  Lab Tests Orders
+                </router-link>
+                <p class="text-dark-50">
+                  Click here to view all laboratory tests orders
+                </p>
+              </div>
+              <div class="ml-6 ml-lg-0 ml-xxl-6 flex-shrink-0">
+                <router-link
+                  to="/orders/laboratory"
+                  class="btn font-weight-bolder text-uppercase btn-light-primary py-4 px-6"
+                >
+                  View
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--end::Callout-->
+      </div>
+      <div class="col-lg-6 col-xl-4 mb-10">
+        <!--begin::Callout-->
+        <div class="card card-custom mb-2 bg-diagonal">
+          <div class="card-body">
+            <div
+              class="d-flex align-items-center justify-content-between p-4 flex-lg-wrap flex-xl-nowrap"
+            >
+              <div class="d-flex flex-column mr-5">
+                <router-link
+                  to="/orders/pharmacy"
+                  class="h4 text-dark text-hover-primary mb-5"
+                >
+                  Pharmacy Orders
+                </router-link>
+                <p class="text-dark-50">
+                  Click here to view all pharmacy orders
+                </p>
+              </div>
+              <div class="ml-6 ml-lg-0 ml-xxl-6 flex-shrink-0">
+                <router-link
+                  to="/orders/pharmacy"
+                  class="btn font-weight-bolder text-uppercase btn-light-primary py-4 px-6"
+                >
+                  View
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--end::Callout-->
+      </div>
+      <div class="col-lg-6 col-xl-4 mb-10">
+        <!--begin::Callout-->
+        <div class="card card-custom mb-2 bg-diagonal">
+          <div class="card-body">
+            <div
+              class="d-flex align-items-center justify-content-between p-4 flex-lg-wrap flex-xl-nowrap"
+            >
+              <div class="d-flex flex-column mr-5">
+                <router-link
+                  to="/orders/radiology"
+                  class="h4 text-dark text-hover-primary mb-5"
+                >
+                  Radiology Orders
+                </router-link>
+                <p class="text-dark-50">
+                  Click here to view all radiology orders
+                </p>
+              </div>
+              <div class="ml-6 ml-lg-0 ml-xxl-6 flex-shrink-0">
+                <router-link
+                  to="/orders/radiology"
+                  class="btn font-weight-bolder text-uppercase btn-light-primary py-4 px-6"
+                >
+                  View
+                </router-link>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!--end::Callout-->
+      </div>
+    </div>
+    <!--end::Row-->
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
