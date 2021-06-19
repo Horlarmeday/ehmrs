484e82be2f8363a5e8afa76325a31909622632af
Mahmud <ajaomahmud@gmail.com>
2021-06-19 16:45:37 +0100

    chore: Removed unused methods

--- server/src/modules/Patient/patient.controller.js
+++ server/src/modules/Patient/patient.controller.js
@@ -6,7 +6,7 @@ import {
   validateOrdinaryPatient,
 } from './validations';
 import PatientService from './patient.service';
-import { getOneDependant, getOnePatient } from './patient.repository';
+import { getOnePatient } from './patient.repository';
 import { APIError } from '../../util/apiError';
 
 class PatientController {
@@ -141,28 +141,6 @@ class PatientController {
   }
 
   /**
-   * get dependants
-   *
-   * @static
-   * @param {object} req express request object
-   * @param {object} res express response object
-   * @param {object} next next middleware
-   * @returns {json} json object with status, dependants data
-   */
-  static async getDependants(req, res, next) {
-    try {
-      const dependants = await PatientService.getDependants(req.query);
-
-      return res.status(200).json({
-        message: 'Success',
-        data: dependants,
-      });
-    } catch (e) {
-      return next(e);
-    }
-  }
-
-  /**
    * update a patient record
    *
    * @static
@@ -188,31 +166,6 @@ class PatientController {
   }
 
   /**
-   * update a dependant record
-   *
-   * @static
-   * @param {object} req express request object
-   * @param {object} res express response object
-   * @param {object} next next middleware
-   * @returns {json} json object with status, dependant data
-   */
-  static async updateDependant(req, res, next) {
-    const { dependant_id } = req.body;
-    if (!dependant_id) throw new APIError('ERROR', 400, 'dependant id is required');
-
-    try {
-      const dependant = await PatientService.updateDependantService(req.body);
-
-      return res.status(200).json({
-        message: 'Successful, data updated!',
-        data: dependant,
-      });
-    } catch (e) {
-      return next(e);
-    }
-  }
-
-  /**
    * get a patient record
    *
    * @static
@@ -231,31 +184,6 @@ class PatientController {
       return res.status(201).json({
         message: 'Success',
         data: patient,
-      });
-    } catch (e) {
-      return next(e);
-    }
-  }
-
-  /**
-   * get a dependant record
-   *
-   * @static
-   * @param {object} req express request object
-   * @param {object} res express response object
-   * @param {object} next next middleware
-   * @returns {json} json object with status, dependant data
-   */
-  static async getDependantProfile(req, res, next) {
-    const { id } = req.params;
-    if (!id) throw new APIError('ERROR', 400, 'invalid dependant id');
-
-    try {
-      const dependant = await getOneDependant(id);
-
-      return res.status(201).json({
-        message: 'Success',
-        data: dependant,
       });
     } catch (e) {
       return next(e);
