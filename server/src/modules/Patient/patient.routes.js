25cd0d699ed60e8e4540cf0be4674decf1bd382d
Mahmud <ajaomahmud@gmail.com>
2021-06-19 16:46:08 +0100

    chore: Removed unused routes

--- server/src/modules/Patient/patient.routes.js
+++ server/src/modules/Patient/patient.routes.js
@@ -8,10 +8,7 @@ router.post('/create/health-insurance', verify, PatientController.createHealthIn
 router.post('/create/ordinary', verify, PatientController.createOrdinaryPatient);
 router.post('/create/dependant/:id', verify, PatientController.createDependant);
 router.put('/update/patient', verify, PatientController.updatePatient);
-router.put('/update/dependant', verify, PatientController.updateDependant);
 router.get('/get', verify, PatientController.getPatients);
-router.get('/dependant/get', verify, PatientController.getDependants);
 router.get('/get/:id', verify, PatientController.getPatientProfile);
-router.get('/dependant/get/:id', verify, PatientController.getDependantProfile);
 
 export default router;
