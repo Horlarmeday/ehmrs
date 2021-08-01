0cedfe215ebff4bb7af07872d2752f8b920d4258
Mahmud <ajaomahmud@gmail.com>
2021-08-01 16:38:07 +0100

    refactor: Extracted SVG icon to component

---
+++ client/src/view/pages/patient/create/components/CashCreateButton.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <b-dropdown
+      split
+      text="Start OPD Visit"
+      class="float-right"
+      variant="primary"
+    >
+      <b-dropdown-item href="#">Start IPD Visit</b-dropdown-item>
+      <b-dropdown-item href="#">Start Emergency Visit</b-dropdown-item>
+    </b-dropdown>
+    <button
+      ref="kt-submit"
+      class="btn btn-light-primary font-weight-bold float-right mr-4"
+      @click="createPatient"
+    >
+      Save
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CashCreateButton",
+  // props: {
+  //   createPatient: {
+  //     type: Function,
+  //     required: true
+  //   }
+  // },
+  methods: {
+    createPatient() {
+      this.$emit("createPatient");
+    }
+  }
+};
+</script>
+
+<style scoped></style>
