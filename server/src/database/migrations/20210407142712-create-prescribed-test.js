82b1d307edcb8143eb3a3940e8b9cef6e22311c8
Mahmud <ajaomahmud@gmail.com>
2021-09-06 09:27:13 +0100

    chore: Modify migration script

--- server/src/database/migrations/20210407142712-create-prescribed-test.js
+++ server/src/database/migrations/20210407142712-create-prescribed-test.js
@@ -10,8 +10,10 @@ module.exports = {
       test_id: {
         type: Sequelize.INTEGER,
       },
-      nhis_test_id: {
-        type: Sequelize.INTEGER,
+      test_type: {
+        type: Sequelize.STRING,
+        defaultValue: 'CASH',
+        allowNull: false,
       },
       requester: {
         type: Sequelize.INTEGER,
