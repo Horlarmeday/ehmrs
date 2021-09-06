b42c30c7cb01472a0626057bb6a1f8b192e4bd23
Mahmud <ajaomahmud@gmail.com>
2021-09-06 09:53:20 +0100

    feat: Collect API response messages to a file

---
+++ server/src/modules/AdminSettings/messages/response-messages.ts
@@ -0,0 +1,2 @@
+export const DATA_SAVED = 'Successful! Data saved';
+export const DATA_UPDATED = 'Successful! Data updated';
---
+++ server/src/modules/Staff/messages/response-messages.ts
@@ -0,0 +1,5 @@
+export const ACCOUNT_CREATED = 'Successful, account created!';
+export const INVALID_STAFF_ID = 'Invalid staff id';
+export const SUCCESS = 'Success';
+export const UPDATED_PROFILE = 'Profile updated successfully';
+export const EXISTING_STAFF = 'Staff already exists';
