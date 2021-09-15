4a9cb7080ad930b8f81a5350254486cd12ab38ff
Mahmud <ajaomahmud@gmail.com>
2021-09-15 13:06:01 +0100

    chore: Remove Nhis price column from cash CreateTest

--- client/src/view/pages/laboratory/create/CreateTest.vue
+++ client/src/view/pages/laboratory/create/CreateTest.vue
@@ -32,18 +32,6 @@
         </div>
       </div>
       <div class="form-group row">
-        <label class="col-lg-3 col-form-label">NHIS Price</label>
-        <div class="col-lg-8">
-          <input
-            type="number"
-            class="form-control form-control-sm"
-            placeholder="Optional"
-            v-model="nhis_price"
-            name="price"
-          />
-        </div>
-      </div>
-      <div class="form-group row">
         <label class="col-lg-3 col-form-label">Sample Type</label>
         <div class="col-lg-8">
           <select
@@ -92,7 +80,6 @@ export default {
       name: "",
       price: "",
       test_id: "",
-      nhis_price: "",
       sample_id: "",
       isDisabled: false
     };
@@ -120,14 +107,13 @@ export default {
         this.initValues();
         this.$validator.reset();
       } else {
-        const { id, name, price, sample_id, nhis_price } = JSON.parse(
+        const { id, name, price, sample_id } = JSON.parse(
           JSON.stringify(this.data)
         );
         this.test_id = id;
         this.name = name;
         this.price = price;
         this.sample_id = sample_id;
-        this.nhis_price = nhis_price;
       }
     }
   },
@@ -158,7 +144,6 @@ export default {
             name: this.name,
             price: this.price,
             test_id: this.test_id,
-            nhis_price: this.nhis_price
           };
           // set spinner to submit button
           const submitButton = this.$refs["kt_test_submit"];
@@ -184,7 +169,6 @@ export default {
       this.price = "";
       this.test_id = "";
       this.sample_id = "";
-      this.nhis_price = "";
     }
   },
   created() {
