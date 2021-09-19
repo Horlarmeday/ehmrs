4c8f24e5f77209fa3295ef3a306615fd8b269e65
Mahmud <ajaomahmud@gmail.com>
2021-09-19 08:10:19 +0100

    chore: Modify lab order endpoint

--- server/src/modules/Orders/Laboratory/lab-order.routes.ts
+++ server/src/modules/Orders/Laboratory/lab-order.routes.ts
@@ -3,6 +3,6 @@ import verify from '../../../core/middleware/verify';
 import LabOrderController from './lab-order.controller';
 
 const router = Router();
-router.post('/lab/create/:id', verify, LabOrderController.orderLabTest);
+router.post('/create/:id', verify, LabOrderController.orderLabTest);
 
 export default router;
