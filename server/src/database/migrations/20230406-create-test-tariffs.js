9cb7aacc88279dd57ee85785809a2aeceee18544
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2023-06-05 08:42:47 +0100

    feat: Add drug & test tariff migrations

---
+++ server/src/database/migrations/20230406-create-test-tariffs.js
@@ -0,0 +1,40 @@
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Test_Tariffs', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      hmo_id: {
+        type: Sequelize.INTEGER,
+      },
+      test_id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      insurance_id: {
+        type: Sequelize.INTEGER,
+      },
+      price: {
+        type: Sequelize.DECIMAL(12, 2),
+        allowNull: false,
+      },
+      staff_id: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Test_Tariffs');
+  },
+};
