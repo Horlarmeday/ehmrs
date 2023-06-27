945d3956078bbe8fc9c446a1b2a0ea53c84925c6
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2023-06-27 20:00:34 +0100

    feat: Add medications components

---
+++ client/src/view/pages/consultation/components/medications/Medications.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="flex-row-fluid ml-lg-8">
+    <div class="card card-custom gutter-b">
+      <div class="card-header pt-5">
+        <h3 class="card-title align-items-start flex-column">
+          <span class="card-label font-weight-bolder text-dark">Medications</span>
+        </h3>
+      </div>
+      <div class="card-body py-2">
+        <div>MEdics</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Medications"
+};
+</script>
+
+<style scoped>
+.flex-row-fluid {
+  -webkit-box-flex: 1;
+  flex: 3 auto;
+  -ms-flex: 1 0 0px;
+  min-width: 0;
+}
+</style>
--- client/src/view/pages/consultation/tabs/Medications.vue
+++ client/src/view/pages/consultation/tabs/Medications.vue
@@ -1,10 +1,34 @@
 <template>
-<div></div>
+  <div id="medications">
+    <div class="d-flex flex-column-fluid">
+      <div class=" container ">
+        <div class="d-flex flex-row">
+          <sidebar />
+          <component :is="activeTab" />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Sidebar from '../components/medications/SideBar.vue';
+import Medications from '../components/medications/Medications.vue';
+const TabMapping = {
+  '#medications': Medications
+}
+
 export default {
-  name: "Medications"
+  name: "Medications",
+  data() {
+    return {
+      activeTab: '',
+    };
+  },
+  components: { Sidebar },
+  created() {
+    this.activeTab = TabMapping['#medications'];
+  },
 };
 </script>
 
