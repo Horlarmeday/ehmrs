769823a7204fff3be4dd988a6a2840a6ffa53444
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2023-06-27 20:01:04 +0100

    feat: Add reusable components

--- client/src/utils/ButtonGroup.vue
+++ client/src/utils/ButtonGroup.vue
@@ -2,25 +2,32 @@
   <b-button-group size="md" class="mt-5">
     <b-button variant="outline-secondary">
       <label class="checkbox checkbox-md checkbox-inline">
-        <input type="checkbox" value="1" />
+        <!--        <input type="checkbox" value="1" />-->
         <span class="mr-2" />
-        {{ count }} Selected
+        {{ count ? count : null }} Selected
       </label>
     </b-button>
     <b-button variant="outline-secondary">Edit Items</b-button>
     <b-dropdown text="More Actions" variant="outline-secondary">
-      <b-dropdown-item>Action A</b-dropdown-item>
-      <b-dropdown-item>Action B</b-dropdown-item>
+      <b-dropdown-item @click="dispenseItems">Dispense</b-dropdown-item>
+      <b-dropdown-item>Reorder</b-dropdown-item>
+      <b-dropdown-item>Export</b-dropdown-item>
     </b-dropdown>
   </b-button-group>
 </template>
 
 <script>
+
 export default {
   name: "ButtonGroup.vue",
   props: {
     count: {
-      type: Number
+      type: Number,
+    },
+  },
+  methods: {
+    dispenseItems() {
+      this.$emit('openModal', true);
     }
   }
 };
---
+++ client/src/utils/CalloutCard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="card card-custom mb-2 bg-diagonal">
+    <div class="card-body">
+      <div
+        class="d-flex align-items-center justify-content-between p-4 flex-lg-wrap flex-xl-nowrap"
+      >
+        <div class="d-flex flex-column mr-5">
+          <router-link :to="link" class="h4 text-dark text-hover-primary mb-5">
+            {{ name }}
+          </router-link>
+          <p class="text-dark-50">
+            {{ desc }}
+          </p>
+        </div>
+        <div class="ml-6 ml-lg-0 ml-xxl-6 flex-shrink-0">
+          <router-link
+            :to="link"
+            class="btn font-weight-bolder text-uppercase btn-light-primary py-4 px-6"
+          >
+            View
+          </router-link>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CalloutCard",
+  props: {
+    link: {
+      type: String,
+      required: true
+    },
+    desc: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
