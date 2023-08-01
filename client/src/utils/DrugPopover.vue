a2f95adaf1ac8a692adaa0a704291cef432bbe1f
Mahmud Ajao <39765147+Horlarmeday@users.noreply.github.com>
2023-08-01 07:35:52 +0100

    feat: Add modal for doctor to discharge patient

---
+++ client/src/utils/DrugPopover.vue
@@ -0,0 +1,123 @@
+<template>
+  <b-popover :show="activePrompt" :target="target" triggers="click blur">
+    <template #title>
+      <b-button @click="onClose" class="close" aria-label="Close">
+        <span class="d-inline-block" aria-hidden="true">&times;</span>
+      </b-button>
+      {{ drug?.drug?.name }}
+    </template>
+    <table class="table">
+      <tbody>
+        <tr>
+          <th scope="row">Drug Type</th>
+          <td>
+            <span>
+              <span :class="getDrugTypeStatus(drug.drug_type)" class="label label-dot mr-2"></span
+              ><span :class="getDrugTypeTextColor(drug.drug_type)" class="font-weight-bold">{{
+                drug.drug_type
+              }}</span>
+            </span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Route</th>
+          <td>
+            <span>{{ drug.route?.name }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Qty Prescribed</th>
+          <td>
+            <span>{{ drug.quantity_prescribed }} {{ drug?.dosage_form?.name }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Dispense Status</th>
+          <td>
+            <span :class="getDispenseStatus(drug.dispense_status)" class="label label-inline"
+              >Pending</span
+            >
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Total Price</th>
+          <td>
+            <span>₦{{ drug.total_price }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Payment Status</th>
+          <td>
+            <span
+              :class="getPaymentStatus(drug.payment_status)"
+              class="label label-inline"
+              >Pending</span
+            >
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Notes</th>
+          <td>
+            <span>{{ drug.notes }}</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </b-popover>
+</template>
+
+<script>
+export default {
+  props: {
+    target: {
+      type: String,
+      required: true,
+    },
+    drug: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+    show: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    activePrompt: {
+      get() {
+        return this.show;
+      },
+      set(value) {
+        this.$emit('closePopover', value);
+      },
+    },
+  },
+  methods: {
+    onClose() {
+      this.$emit('closePopover', false);
+    },
+    getDispenseStatus(status) {
+      if (status === 'Pending') return 'label-light-warning';
+      if (status === 'Dispensed') return 'label-light-success';
+      if (status === 'Returned') return 'label-light-info';
+    },
+    getPaymentStatus(status) {
+      if (status === 'Pending') return 'label-light-warning font-weight-bold';
+      if (status === 'Paid') return 'label-light-success font-weight-bold';
+      if (status === 'Cleared') return 'label-light-info font-weight-bold';
+      if (status === 'Permitted') return 'label-light-danger font-weight-bold';
+    },
+    getDrugTypeStatus(type) {
+      if (type === 'NHIS') return 'label-danger';
+      if (type === 'Cash') return 'label-success';
+    },
+    getDrugTypeTextColor(type) {
+      if (type === 'NHIS') return 'text-danger';
+      if (type === 'Cash') return 'text-success';
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ client/src/view/pages/consultation/tabs/Disposition.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="disposition">
+    <admission />
+  </div>
+</template>
+
+<script>
+import Admission from '@/view/pages/consultation/components/disposition/admission/Admission.vue';
+
+export default {
+  components: { Admission },
+};
+</script>
+
+<style scoped></style>
