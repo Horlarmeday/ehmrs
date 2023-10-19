6744f14a1d3470e4c90fef7de534e5156aa2fee9
Mahmud Ajao <ajaomahmud@gmail.com>
2023-10-19 09:32:38 +0100

    chore: Run prettier

--- client/src/core/services/store/auth/moduleAuthActions.js
+++ client/src/core/services/store/auth/moduleAuthActions.js
@@ -1,26 +1,26 @@
 /* eslint-disable no-unused-vars */
-import axios from "axios";
+import axios from 'axios';
 
 export default {
   login({ commit }, staff) {
     return new Promise((resolve, reject) => {
-      commit("auth_request");
+      commit('auth_request');
       axios({
         url: `/auth/login`,
         data: staff,
-        method: "POST"
+        method: 'POST',
       })
         .then(response => {
           const token = response.data.data;
-          localStorage.setItem("user_token", token);
+          localStorage.setItem('user_token', token);
 
-          axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-          commit("auth_success", token);
+          axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+          commit('auth_success', token);
           resolve(response);
         })
         .catch(err => {
-          commit("auth_error");
-          localStorage.removeItem("user_token");
+          commit('auth_error');
+          localStorage.removeItem('user_token');
           reject(err);
         });
     });
@@ -28,23 +28,23 @@ export default {
 
   register({ commit }, user) {
     return new Promise((resolve, reject) => {
-      commit("auth_request");
+      commit('auth_request');
       axios({
-        url: "/users/register",
+        url: '/users/register',
         data: user,
-        method: "POST"
+        method: 'POST',
       })
         .then(response => {
           const token = response.data.token;
           const user = response.data.data;
-          localStorage.setItem("user_token", token);
-          axios.defaults.headers.common["authorization"] = `Bearer ${token}`;
-          commit("auth_success", token, user);
+          localStorage.setItem('user_token', token);
+          axios.defaults.headers.common['authorization'] = `Bearer ${token}`;
+          commit('auth_success', token, user);
           resolve(response);
         })
         .catch(err => {
-          commit("auth_error", err);
-          localStorage.removeItem("user_token");
+          commit('auth_error', err);
+          localStorage.removeItem('user_token');
           reject(err);
         });
     });
@@ -52,19 +52,19 @@ export default {
 
   forgot({ commit }, phone) {
     return new Promise((resolve, reject) => {
-      commit("auth_request");
+      commit('auth_request');
       axios({
-        url: "/auth/forgot-password",
+        url: '/auth/forgot-password',
         data: phone,
-        method: "POST"
+        method: 'POST',
       })
         .then(response => {
           const user = response.data.data;
-          commit("auth_success", user);
+          commit('auth_success', user);
           resolve(response);
         })
         .catch(err => {
-          commit("auth_error", err);
+          commit('auth_error', err);
           reject(err);
         });
     });
@@ -72,11 +72,11 @@ export default {
 
   logout({ commit }) {
     return new Promise((resolve, reject) => {
-      commit("logout");
-      localStorage.removeItem("user_token");
-      delete axios.defaults.headers.common["authorization"];
+      commit('logout');
+      localStorage.removeItem('user_token');
+      delete axios.defaults.headers.common['authorization'];
       window.location.replace('/');
       resolve();
     });
-  }
+  },
 };
