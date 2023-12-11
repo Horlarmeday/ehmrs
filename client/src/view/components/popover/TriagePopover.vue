77445057e7cfc07b3dec65e148719d4b83402ed1
Mahmud Ajao <ajaomahmud@gmail.com>
2023-12-11 08:31:08 +0100

    feat: Add triage table

---
+++ client/src/view/components/popover/TriagePopover.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-popover :show="activePrompt" :target="target" triggers="click blur">
+    <b-button @click="onClose" class="close text-danger" aria-label="Close">
+      <span class="d-inline-block" aria-hidden="true">&times;</span>
+    </b-button>
+    <table class="table table-sm">
+      <tbody>
+        <tr>
+          <th scope="row">Pulse</th>
+          <td>
+            <span>{{ triage.pulse || '-' }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Respiration</th>
+          <td>
+            <span>{{ triage.respiration || '-' }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">Heart Rate</th>
+          <td>
+            <span>{{ triage.heart_rate || '-' }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">MUAC</th>
+          <td>
+            <span>{{ triage.muac || '-' }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">SPO2</th>
+          <td>
+            <span>{{ triage.spo2 || '-' }}</span>
+          </td>
+        </tr>
+        <tr>
+          <th scope="row">RVS</th>
+          <td>
+            <span>{{ triage.rvs || '-' }}</span>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </b-popover>
+</template>
+
+<script>
+export default {
+  props: {
+    target: {
+      type: String,
+      required: true,
+    },
+    triage: {
+      type: Object,
+      required: true,
+      default: () => {},
+    },
+    show: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  computed: {
+    activePrompt: {
+      get() {
+        return this.show;
+      },
+      set(value) {
+        this.$emit('closePopover', value);
+      },
+    },
+  },
+  methods: {
+    onClose() {
+      this.$emit('closePopover', false);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
