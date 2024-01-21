e2de798c342d018baf50eb013281a213ac6be39f
Mahmud Ajao <ajaomahmud@gmail.com>
2024-01-21 23:49:59 +0100

    feat: Add skeleton

---
+++ client/src/view/pages/nhis/components/TableSkeleton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <b-skeleton-table :rows="5" :columns="columns" :table-props="{ striped: true }" />
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    rows: {
+      type: Number,
+      default: 5,
+    },
+    columns: {
+      type: Number,
+      required: true,
+      default: 5,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ client/src/view/pages/programs/immunization/components/TabsSkeleton.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="example">
+    <b-tabs content-class="mt-3">
+      <b-tab v-for="(tab, i) in tabs" :key="i" :title="tab" active>
+        <DefaultSkeleton />
+      </b-tab>
+    </b-tabs>
+  </div>
+</template>
+<script>
+import DefaultSkeleton from '@/utils/DefaultSkeleton.vue';
+
+export default {
+  components: { DefaultSkeleton },
+  props: {
+    tabs: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped></style>
