372be5890a57515dfb7dd47b13eb509da9211128
Mahmud Ajao <ajaomahmud@gmail.com>
2024-01-21 23:46:41 +0100

    chore: Remove unecessary code

--- client/src/view/pages/admission/page/Treatment.vue
+++ client/src/view/pages/admission/page/Treatment.vue
@@ -7,54 +7,20 @@
     </div>
     <div class="card-body">
       <treatment-accordion :filter="filter" />
-      <add-treatment
-        source="Admission"
-        :show-switch="showSwitch"
-        :switch-position="switchPosition"
-        :filter="filter"
-      />
+      <add-treatment source="Admission" :filter="filter" />
     </div>
   </div>
 </template>
 <script>
-import { EXCLUDED_INSURANCE } from '@/common/common';
 import TreatmentAccordion from '@/view/components/accordion/TreatmentAccordion.vue';
 import AddTreatment from '@/view/pages/visits/components/tabs/treatments/AddTreatment.vue';
 
 export default {
   components: { AddTreatment, TreatmentAccordion },
-  data: () => ({
-    switchPosition: false,
-  }),
   computed: {
-    admission() {
-      return this.$store.state.admission.admission;
-    },
     filter() {
       return { admission_id: this.$route.params.id };
     },
-    showSwitch() {
-      return (
-        (this.admission?.patient?.has_insurance &&
-          !EXCLUDED_INSURANCE.includes(this.admission?.insurance?.insurance?.name)) ||
-        false
-      );
-    },
-  },
-  methods: {
-    defaultSwitchPosition() {
-      setTimeout(() => {
-        if (
-          this.admission?.patient?.has_insurance &&
-          !EXCLUDED_INSURANCE.includes(this.admission?.insurance?.insurance?.name)
-        ) {
-          this.switchPosition = true;
-        }
-      }, 350);
-    },
-  },
-  created() {
-    this.defaultSwitchPosition();
   },
 };
 </script>
--- client/src/view/pages/visits/components/tabs/treatments/Treatments.vue
+++ client/src/view/pages/visits/components/tabs/treatments/Treatments.vue
@@ -7,17 +7,11 @@
     </div>
     <div class="card-body">
       <treatment-accordion :filter="filter" />
-      <add-treatment
-        source="Consultation"
-        :show-switch="showSwitch"
-        :switch-position="switchPosition"
-        :filter="filter"
-      />
+      <add-treatment source="Consultation" :filter="filter" />
     </div>
   </div>
 </template>
 <script>
-import { EXCLUDED_INSURANCE } from '@/common/common';
 import TreatmentAccordion from '@/view/components/accordion/TreatmentAccordion.vue';
 import AddTreatment from '@/view/pages/visits/components/tabs/treatments/AddTreatment.vue';
 
@@ -27,34 +21,9 @@ export default {
     switchPosition: false,
   }),
   computed: {
-    visit() {
-      return this.$store.state.visit.visit;
-    },
     filter() {
       return { visit_id: this.$route.params.id };
     },
-    showSwitch() {
-      return (
-        (this.visit?.patient?.has_insurance &&
-          !EXCLUDED_INSURANCE.includes(this.visit?.insurance?.insurance?.name)) ||
-        false
-      );
-    },
-  },
-  methods: {
-    defaultSwitchPosition() {
-      setTimeout(() => {
-        if (
-          this.visit?.patient?.has_insurance &&
-          !EXCLUDED_INSURANCE.includes(this.visit?.insurance?.insurance?.name)
-        ) {
-          this.switchPosition = true;
-        }
-      }, 350);
-    },
-  },
-  created() {
-    this.defaultSwitchPosition();
   },
 };
 </script>
