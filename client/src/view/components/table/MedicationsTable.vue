6657da4135b1905f8c5733af447140173627c1f8
Mahmud Ajao <ajaomahmud@gmail.com>
2024-06-19 22:47:34 +0100

    feat: Add check if patient has insurance

--- client/src/view/components/table/MedicationsTable.vue
+++ client/src/view/components/table/MedicationsTable.vue
@@ -26,6 +26,13 @@
                 class="label label-dot label-lg mr-2"
               ></span>
               <a @click="viewPopover(drug)" href="#" :id="popOverId"> {{ drug.drug.name }}</a>
+              <span class="ml-2">
+                <label
+                  v-if="drug?.drug_group"
+                  class="label label-sm label-inline label-secondary"
+                  >{{ drug?.drug_group }}</label
+                >
+              </span>
             </th>
             <td>
               <span>{{ drug.quantity_to_dispense }} {{ drug?.dosage_form?.name || '-' }}</span>
