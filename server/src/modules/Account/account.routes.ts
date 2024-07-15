dfa82bf5d1d0aac87ea803ea024b1a1cc5c82d3d
Mahmud Ajao <ajaomahmud@gmail.com>
2024-07-15 22:02:21 +0100

    feat: Add endpoint to fetch patient payment history

--- server/src/modules/Account/account.routes.ts
+++ server/src/modules/Account/account.routes.ts
@@ -4,5 +4,6 @@ import { AccountController } from './account.controller';
 
 const router = Router();
 router.post('/payment/create', verify, AccountController.createPaymentHistory);
+router.get('/payment-history/:id', verify, AccountController.getPatientPaymentHistory);
 
 export default router;
