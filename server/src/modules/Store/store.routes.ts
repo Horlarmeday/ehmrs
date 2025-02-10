cff05e00a5b5de781568c0060c5f5b93364e22a9
Mahmud Ajao <ajaomahmud@gmail.com>
2025-02-10 10:05:34 +0100

    fix: Fix store items approval

--- server/src/modules/Store/store.routes.ts
+++ server/src/modules/Store/store.routes.ts
@@ -8,8 +8,10 @@ router.post('/pharmacy/items/dispense', verify, StoreController.dispenseStoreIte
 router.post('/pharmacy/items/reorder', verify, StoreController.reorderStoreItems);
 router.post('/pharmacy/items/export', verify, StoreController.exportData);
 router.post('/laboratory/items/create', verify, StoreController.createLaboratoryItem);
+router.post('/pharmacy/vendors/create', verify, StoreController.createVendor);
 router.get('/pharmacy/selected-items', verify, StoreController.getSelectedPharmacyStoreItems);
 router.get('/pharmacy/items/get', verify, StoreController.getPharmacyStoreItems);
+router.get('/pharmacy/vendors/get', verify, StoreController.getVendors);
 router.get('/pharmacy/items/:id', verify, StoreController.getPharmacyStoreItem);
 router.get('/pharmacy/items/history/:id', verify, StoreController.getPharmacyStoreItemHistory);
 router.get('/pharmacy/items/logs/:id', verify, StoreController.getPharmacyStoreItemLogs);
@@ -17,5 +19,6 @@ router.get('/laboratory/items/get', verify, StoreController.getLaboratoryItems);
 router.put('/pharmacy/items/deactivate', verify, StoreController.deactivatePharmacyItems);
 router.put('/pharmacy/items/update', verify, StoreController.updatePharmacyItems);
 router.put('/pharmacy/items/reset', verify, StoreController.resetPharmacyStoreItemsQuantity);
+router.put('/pharmacy/vendors/update/:id', verify, StoreController.updateVendor);
 
 export default router;
